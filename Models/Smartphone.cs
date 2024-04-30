6d755b9e4a2db954656cdc2f2caeb4531616e32b
Carlos Araujo <carlosed.araujo@gmail.com>
2024-04-30 10:21:20 -0300

    DesafioPOO - Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,24 +1,43 @@
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        // IMPLEMENTADO
 
-        public Smartphone(string numero)
+        private string Modelo { get; set; }
+
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            // IMPLEMENTADO
+
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            //refatorando...
+            Console.WriteLine($"Ligando... {Numero}");
+            Thread.Sleep(2000);
+          
         }
+
+       
 
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+            Thread.Sleep(2000);
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Music Player");
+
+            Iphone iphone = new Iphone("987654321", "iPhone 12", "987654321", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("WhatsApp");
+
+            Nokia nokia2 = new Nokia("2499999-9878", "Nokia 3380", "3902387490", 256);
+            nokia2.Ligar();
+            nokia2.ReceberLigacao();
+            nokia2.InstalarAplicativo("Facebook");
+
+            Iphone iphone2 = new Iphone("2499999-9878", "iPhone 15", "3902387490", 256);
+            iphone2.Ligar();
+            iphone2.ReceberLigacao();
+            iphone2.InstalarAplicativo("Instagram");
+
+
+        }
+    }
+}
